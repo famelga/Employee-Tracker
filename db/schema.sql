ebed8df7270b9c0bc67586cb9542fe099543a0f4
Fayven <fayven.amelga@gmail.com>
2023-01-31 21:56:27 -0500

    Add table values for schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,25 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-
+    id INT NOT NULL,
+    department_name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
+    -- REFERENCES dept id
 );
 
 CREATE TABLE employee (
-
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT DEFAULT false,
+    PRIMARY KEY (id)
 );

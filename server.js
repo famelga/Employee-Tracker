4848792c6b3128050d62ed2b682c098c79a8b30b
Fayven <fayven.amelga@gmail.com>
2023-01-31 22:05:10 -0500

    Add const db to connect to mysql

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const mysql = require('mysql2');
 
 // const PORT = ;
 // const app = express();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'tracker_db'
+    },
+    console.log('Connected to the tracker_db.')
+);
